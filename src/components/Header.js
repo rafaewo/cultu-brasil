6b72a6bab159b9f98df69707153ec255f9e79b1a
rafaewo <rafaelnogcunha@hotmail.com>
2019-10-20 18:51:29 -0300

    finalizando estilização

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './style.css'
 
 export default function Header() {
 	return(
 		<div>
 			<h1 className='f1 fw8 ma0 near-white'>CultuBrasil</h1>
+			<h3 className='ttu tracked fw1 f5 white'>celebre a diversidade</h3>
 		</div>
 	);
 }
