d0648358f119df1a384d14984a112d9566bafa3a
rafaewo <rafaelnogcunha@hotmail.com>
2019-10-26 01:56:58 -0300

    Updates

---
+++ precache-manifest.ece3bf241e0fd27199d1b7b114514246.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1cf64156e43f2c4da321063100c228",
+    "url": "/cultu-brasil/index.html"
+  },
+  {
+    "revision": "c752ce0a9e72f7d1a744",
+    "url": "/cultu-brasil/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6d1a7f8ee1a88eb3eec3",
+    "url": "/cultu-brasil/static/css/main.3bd4b6d8.chunk.css"
+  },
+  {
+    "revision": "c752ce0a9e72f7d1a744",
+    "url": "/cultu-brasil/static/js/2.24e6ed16.chunk.js"
+  },
+  {
+    "revision": "6d1a7f8ee1a88eb3eec3",
+    "url": "/cultu-brasil/static/js/main.552fab07.chunk.js"
+  },
+  {
+    "revision": "197e6c6de8f8f7e1372c",
+    "url": "/cultu-brasil/static/js/runtime-main.cf86440a.js"
+  },
+  {
+    "revision": "0192b4f082a0d95e5336b2dea6af2c10",
+    "url": "/cultu-brasil/static/media/brasil.0192b4f0.svg"
+  },
+  {
+    "revision": "90934a0c8dc25ebd0c3884d447b5bc12",
+    "url": "/cultu-brasil/static/media/feather1.90934a0c.svg"
+  }
+]);
